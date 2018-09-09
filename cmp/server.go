2385ce33794ac7822c4f95ae3afe8a95c4c842c5
Jon Calhoun <joncalhoun@gmail.com>
2018-09-09 07:44:51 -0400

    Added a comparison helper function example

---
+++ cmp/server.go
@@ -0,0 +1,62 @@
+package cmp
+
+import (
+	"fmt"
+	"net/http"
+	"sync"
+)
+
+type App struct {
+	sync.Once
+	mux http.ServeMux
+}
+
+func (a *App) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	a.Once.Do(func() {
+		a.mux = http.ServeMux{}
+		a.mux.HandleFunc("/", a.Home)
+		a.mux.HandleFunc("/alert", a.WithAlert)
+		a.mux.HandleFunc("/many", a.ManyAlerts)
+	})
+	a.mux.ServeHTTP(w, r)
+}
+
+func (a App) ManyAlerts(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, `
+	<html>
+		<head><title>Alert Page</title></head>
+		<body>
+			<div class="alert alert-primary" role="alert">
+				Alert Number 1
+			</div>
+			<div class="alert alert-primary" role="alert">
+				Alert Number 2
+			</div>
+			<h1>Alert page</h1>
+		</body>
+	</html>`)
+}
+
+func (a App) WithAlert(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, `
+	<html>
+		<head><title>Alert Page</title></head>
+		<body>
+			<div class="alert alert-primary" role="alert">
+				Stuff went wrong!
+			</div>
+			<h1>Alert page</h1>
+		</body>
+	</html>`)
+}
+
+func (a App) Home(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, `
+	<html>
+		<head><title>Home Page</title></head>
+		<body>
+			<h1>Welcome</h1>
+			<p>This is the home page</p>
+		</body>
+	</html>`)
+}
---
+++ cmp/server_test.go
@@ -0,0 +1,117 @@
+package cmp_test
+
+import (
+	"fmt"
+	"io"
+	"net/http"
+	"net/http/httptest"
+	"strings"
+	"testing"
+
+	"github.com/joncalhoun/twg/cmp"
+	"golang.org/x/net/html"
+)
+
+func getText(n *html.Node) string {
+	var ret []string
+	for c := n.FirstChild; c != nil; c = c.NextSibling {
+		switch c.Type {
+		case html.TextNode:
+			ret = append(ret, strings.TrimSpace(c.Data))
+		case html.ElementNode:
+			getText(c)
+		}
+	}
+	return strings.Join(ret, " ")
+}
+
+func findNodes(body, tag, class string) ([]string, error) {
+	// Source adapted from the html package's examples
+	doc, err := html.Parse(strings.NewReader(body))
+	if err != nil {
+		return nil, err
+	}
+	var ret []string
+	var find func(n *html.Node)
+	find = func(n *html.Node) {
+		if n.Type == html.ElementNode && n.Data == tag {
+			for _, a := range n.Attr {
+				if a.Key == "class" {
+					classes := strings.Fields(a.Val)
+					for _, c := range classes {
+						if c == class {
+							ret = append(ret, getText(n))
+						}
+					}
+					break
+				}
+			}
+		}
+		for c := n.FirstChild; c != nil; c = c.NextSibling {
+			find(c)
+		}
+	}
+	find(doc)
+	return ret, nil
+}
+
+func TestApp(t *testing.T) {
+	app := cmp.App{}
+
+	type checkFn func(r *http.Response, body string) error
+	hasNoAlerts := func() func(*http.Response, string) error {
+		return func(r *http.Response, body string) error {
+			nodes, err := findNodes(body, "div", "alert")
+			if err != nil {
+				return fmt.Errorf("findNodes() err = %s", err)
+			}
+			if len(nodes) != 0 {
+				return fmt.Errorf("len(alerts)=%d; want len(alerts)=0", len(nodes))
+			}
+			return nil
+		}
+	}
+	hasAlert := func(msg string) func(*http.Response, string) error {
+		return func(r *http.Response, body string) error {
+			nodes, err := findNodes(body, "div", "alert")
+			if err != nil {
+				return fmt.Errorf("findNodes() err = %s", err)
+			}
+			for _, node := range nodes {
+				if node == msg {
+					return nil
+				}
+			}
+			return fmt.Errorf("missing alert: %q", msg)
+		}
+	}
+
+	tests := []struct {
+		method string
+		path   string
+		body   io.Reader
+		checks []checkFn
+	}{
+		{http.MethodGet, "/", nil, []checkFn{hasNoAlerts()}},
+		{http.MethodGet, "/alert", nil, []checkFn{hasAlert("Stuff went wrong!")}},
+		{http.MethodGet, "/many", nil, []checkFn{hasAlert("Alert Number 1"), hasAlert("Alert Number 2")}},
+	}
+	for _, tc := range tests {
+		t.Run(fmt.Sprintf("%s %s", tc.method, tc.path), func(t *testing.T) {
+			w := httptest.NewRecorder()
+			r, err := http.NewRequest(tc.method, tc.path, nil)
+			if err != nil {
+				t.Fatalf("http.NewRequest() err = %s", err)
+			}
+			app.ServeHTTP(w, r)
+			res := w.Result()
+			var sb strings.Builder
+			io.Copy(&sb, res.Body)
+			for _, check := range tc.checks {
+				if err := check(res, sb.String()); err != nil {
+					t.Error(err)
+				}
+			}
+		})
+	}
+}
