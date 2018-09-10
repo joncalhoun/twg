278c1f1f6bd27633d660abf0e7808d4182157df5
Jon Calhoun <joncalhoun@gmail.com>
2018-09-10 14:39:00 -0400

    Uncommenting parallel tests

--- parallel/parallel_test.go
+++ parallel/parallel_test.go
@@ -3,37 +3,38 @@ package parallel
 import (
 	"fmt"
 	"testing"
+	"time"
 )
 
-// func TestSomething(t *testing.T) {
-// 	t.Parallel()
-// 	time.Sleep(time.Second)
-// }
+func TestSomething(t *testing.T) {
+	t.Parallel()
+	time.Sleep(time.Second)
+}
 
-// func TestA(t *testing.T) {
-// 	t.Parallel()
-// 	time.Sleep(time.Second)
-// }
+func TestA(t *testing.T) {
+	t.Parallel()
+	time.Sleep(time.Second)
+}
 
-// func TestB(t *testing.T) {
-// 	fmt.Println("setup")
-// 	defer fmt.Println("deferred teardown")
-// 	t.Run("group", func(t *testing.T) {
-// 		t.Run("sub1", func(t *testing.T) {
-// 			t.Parallel()
-// 			// run sub1
-// 			time.Sleep(time.Second)
-// 			fmt.Println("sub1 done")
-// 		})
-// 		t.Run("sub2", func(t *testing.T) {
-// 			t.Parallel()
-// 			// run sub2
-// 			time.Sleep(time.Second)
-// 			fmt.Println("sub2 done")
-// 		})
-// 	})
-// 	fmt.Println("teardown")
-// }
+func TestB(t *testing.T) {
+	fmt.Println("setup")
+	defer fmt.Println("deferred teardown")
+	t.Run("group", func(t *testing.T) {
+		t.Run("sub1", func(t *testing.T) {
+			t.Parallel()
+			// run sub1
+			time.Sleep(time.Second)
+			fmt.Println("sub1 done")
+		})
+		t.Run("sub2", func(t *testing.T) {
+			t.Parallel()
+			// run sub2
+			time.Sleep(time.Second)
+			fmt.Println("sub2 done")
+		})
+	})
+	fmt.Println("teardown")
+}
 
 func TestGotcha(t *testing.T) {
 	testCases := []struct {
