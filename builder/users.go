ebfbd76c64c3f15310fd39dadae8400c1526ef28
Jon Calhoun <joncalhoun@gmail.com>
2018-09-10 13:40:20 -0400

    Added the builder test samples

---
+++ builder/users.go
@@ -0,0 +1,71 @@
+package builder
+
+import (
+	"database/sql"
+
+	"github.com/pkg/errors"
+)
+
+// Common errors that you will likely want to account for in your code.
+// Any other errors are wrapped with context vai the github.com/pkg/errors
+// package and returned but are harder to use an if/switch to match.
+var (
+	ErrNotFound = errors.New("builder: resource could not be located")
+)
+
+// User is an example user model. This typically wouldn't be defined in
+// this package but is done here for simplicity.
+type User struct {
+	ID    int
+	Name  string
+	Email string
+}
+
+// UserStore is used to interact with our user store.
+type UserStore struct {
+	sql interface {
+		Exec(query string, args ...interface{}) (sql.Result, error)
+		QueryRow(query string, args ...interface{}) *sql.Row
+	}
+}
+
+// Find will retrieve a user with the provided ID or return ErrNotFount
+// if the user isn't located. Other errors are wrapped with context
+// but are otherwise wrapped as-is.
+func (us *UserStore) Find(id int) (*User, error) {
+	const query = `SELECT id, name, email FROM users WHERE id=$1;`
+	row := us.sql.QueryRow(query, id)
+	var user User
+	err := row.Scan(&user.ID, &user.Name, &user.Email)
+	switch err {
+	case sql.ErrNoRows:
+		return nil, ErrNotFound
+	case nil:
+		return &user, nil
+	default:
+		return nil, errors.Wrap(err, "builder: error querying for user by id")
+	}
+}
+
+// Create will create a new user in the DB using the provided user and
+// will update the ID of the provided user. If there is an error it will
+// be wrapped and returned.
+func (us *UserStore) Create(user *User) error {
+	const query = `INSERT INTO users (name, email) VALUES ($1, $2) RETURNING id`
+	err := us.sql.QueryRow(query, user.Name, user.Email).Scan(&user.ID)
+	if err != nil {
+		return errors.Wrap(err, "builder: error creating new user")
+	}
+	return nil
+}
+
+// Delete will delete a user form the DB. If there is an error it will
+// be wrapped and returned.
+func (us *UserStore) Delete(id int) error {
+	const query = `DELETE FROM users WHERE id=$1;`
+	_, err := us.sql.Exec(query, id)
+	if err != nil {
+		return errors.Wrap(err, "builder: error deleting user")
+	}
+	return nil
+}
---
+++ builder/users_test.go
@@ -0,0 +1,127 @@
+package builder
+
+import (
+	"database/sql"
+	"fmt"
+	"os"
+	"reflect"
+	"testing"
+
+	_ "github.com/lib/pq"
+)
+
+func TestMain(m *testing.M) {
+	// 0. flag.Parse() if you need flags
+	exitCode := run(m)
+	os.Exit(exitCode)
+}
+
+func run(m *testing.M) int {
+	const (
+		dropDB          = `DROP DATABASE IF EXISTS test_user_store;`
+		createDB        = `CREATE DATABASE test_user_store;`
+		createUserTable = `CREATE TABLE users (
+												 id SERIAL PRIMARY KEY,
+												 name TEXT,
+												 email TEXT UNIQUE NOT NULL
+											 );`
+	)
+
+	builder, err := sql.Open("postgres", "host=localhost port=5432 user=jon sslmode=disable")
+	if err != nil {
+		panic(fmt.Errorf("sql.Open() err = %s", err))
+	}
+	defer builder.Close()
+
+	_, err = builder.Exec(dropDB)
+	if err != nil {
+		panic(fmt.Errorf("builder.Exec() err = %s", err))
+	}
+	_, err = builder.Exec(createDB)
+	if err != nil {
+		panic(fmt.Errorf("builder.Exec() err = %s", err))
+	}
+	// teardown
+	defer func() {
+		_, err = builder.Exec(dropDB)
+		if err != nil {
+			panic(fmt.Errorf("builder.Exec() err = %s", err))
+		}
+	}()
+
+	db, err := sql.Open("postgres", "host=localhost port=5432 user=jon sslmode=disable dbname=test_user_store")
+	if err != nil {
+		panic(fmt.Errorf("sql.Open() err = %s", err))
+	}
+	defer db.Close()
+	_, err = db.Exec(createUserTable)
+	if err != nil {
+		panic(fmt.Errorf("db.Exec() err = %s", err))
+	}
+
+	return m.Run()
+}
+
+func userStore(t *testing.T) (*UserStore, func()) {
+	t.Helper()
+	db, err := sql.Open("postgres", "host=localhost port=5432 user=jon sslmode=disable dbname=test_user_store")
+	if err != nil {
+		t.Fatalf("sql.Open() err = %s", err)
+		return nil, nil
+	}
+	us := &UserStore{
+		sql: db,
+	}
+	return us, func() {
+		db.Close()
+	}
+}
+
+func TestUserStore(t *testing.T) {
+	us, teardown := userStore(t)
+	defer teardown()
+	t.Run("Find", testUserStore_Find(us))
+	t.Run("Create", testUserStore_Find(us))
+	t.Run("Delete", testUserStore_Find(us))
+	t.Run("Subscribe", testUserStore_Find(us))
+}
+
+func testUserStore_Find(us *UserStore) func(t *testing.T) {
+	return func(t *testing.T) {
+		jon := &User{
+			Name:  "Jon Calhoun",
+			Email: "jon@calhoun.io",
+		}
+		err := us.Create(jon)
+		if err != nil {
+			t.Errorf("us.Create() err = %s", err)
+		}
+		defer func() {
+			err := us.Delete(jon.ID)
+			if err != nil {
+				t.Errorf("us.Delete() err = %s", err)
+			}
+		}()
+
+		tests := []struct {
+			name    string
+			id      int
+			want    *User
+			wantErr error
+		}{
+			{"Found", jon.ID, jon, nil},
+			{"Not Found", -1, nil, ErrNotFound},
+		}
+		for _, tc := range tests {
+			t.Run(tc.name, func(t *testing.T) {
+				got, err := us.Find(tc.id)
+				if err != tc.wantErr {
+					t.Errorf("us.Find() err = %s", err)
+				}
+				if !reflect.DeepEqual(got, tc.want) {
+					t.Errorf("us.Find() = %+v, want %+v", got, tc.want)
+				}
+			})
+		}
+	}
+}
