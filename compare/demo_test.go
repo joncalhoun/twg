4123377b722c4fc01a64ac9aa9168794e42fc2f6
Jon Calhoun <joncalhoun@gmail.com>
2018-09-10 09:42:20 -0400

    removed unused code

--- compare/demo_test.go
+++ compare/demo_test.go
@@ -1,8 +1,6 @@
 package compare
 
 import (
-	"fmt"
-	"net/http"
 	"reflect"
 	"testing"
 )
@@ -67,60 +65,4 @@ func TestDogWithFn(t *testing.T) {
 	if !reflect.DeepEqual(morty, morty2) {
 		t.Errorf("morty != morty2")
 	}
-}
-
-func TestAddTechniques(t *testing.T) {
-	morty := &DogWithFn{
-		Name: "Morty",
-		Age:  1,
-	}
-	morty2 := &DogWithFn{
-		Name: "Morty",
-		Age:  3,
-	}
-
-	// Third party packages - see go-cmp
-
-	// Check specific attributes you care about
-	if morty.Name != morty2.Name {
-
-	}
-
-	// Helper function example: https://golang.org/src/net/http/httptest/recorder_test.go
-	type checkFn func(*http.Response) error
-	hasStatusRange := func(lo, hi int) checkFn {
-		return func(res *http.Response) error {
-			if res.StatusCode < lo || res.StatusCode > hi {
-				return fmt.Errorf("status code = %d; wanted %d <= code <= %d", res.StatusCode, lo, hi)
-				// status code = 333; wanted 200 <= code <= 299
-			}
-		}
-	}
-
-	tests := []struct {
-		args     string
-		checkFns []checkFn
-	}{
-		// test cases
-		{"args", []checkFn{
-			hasName("morty"),
-			hasStatusCode(200),
-		}},
-		{"args", []checkFn{
-			hasName("morty"),
-			hasStatusRange(200, 299),
-		}},
-	}
-	for _, tc := range tests {
-		t.Run("...", func(t *testing.T) {
-			got := SomeFunc(args)
-			for _, check := range tc.checkFns {
-				err := check(got)
-				if err != nil {
-					t.Error(err)
-				}
-			}
-		})
-	}
-
 }
