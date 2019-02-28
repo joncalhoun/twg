f364d35d417c01f373cde2c381e9c721cf7e1301
Jon Calhoun <joncalhoun@gmail.com>
2019-02-28 09:53:22 -0500

    Added a cvc check fail test

--- swag/http/orders_int_test.go
+++ swag/http/orders_int_test.go
@@ -5,6 +5,7 @@ package http_test
 import (
 	"context"
 	"flag"
+	"io/ioutil"
 	"net/http"
 	"net/http/httptest"
 	"net/url"
@@ -83,6 +84,49 @@ func TestOrderHandler_Create_stripeInt(t *testing.T) {
 		gotStripeCusID := gotLoc[len("/orders/"):]
 		if gotStripeCusID != stripeCustomerID {
 			t.Fatalf("Stripe Customer ID = %s; want %s", gotStripeCusID, stripeCustomerID)
+		}
+	})
+
+	t.Run("cvc check fail", func(t *testing.T) {
+		token := "tok_cvcCheckFail"
+		oh := OrderHandler{}
+		oh.DB = &mockDB{
+			CreateOrderFunc: func(order *db.Order) error {
+				order.ID = 123
+				return nil
+			},
+		}
+		formData := url.Values{
+			"Name":         []string{"Jon Calhoun"},
+			"Email":        []string{"jon@calhoun.io"},
+			"stripe-token": []string{token},
+		}
+		stripeClient := &stripe.Client{
+			Key: stripeSecretKey,
+		}
+		oh.Stripe.Client = stripeClient
+		oh.Logger = &logRec{}
+
+		w := httptest.NewRecorder()
+		r := httptest.NewRequest(http.MethodPost, "/orders", strings.NewReader(formData.Encode()))
+		r.Header.Set("Content-Type", "application/x-www-form-urlencoded")
+		r = r.WithContext(context.WithValue(r.Context(), "campaign", &db.Campaign{
+			ID: 333,
+		}))
+		oh.Create(w, r)
+		res := w.Result()
+		if res.StatusCode != http.StatusInternalServerError {
+			t.Fatalf("StatusCode = %d; want %d", res.StatusCode, http.StatusInternalServerError)
+		}
+		defer res.Body.Close()
+		body, err := ioutil.ReadAll(res.Body)
+		if err != nil {
+			t.Fatalf("ReadAll() err = %v; want %v", err, nil)
+		}
+		gotBody := strings.TrimSpace(string(body))
+		wantBody := "Something went wrong processing your payment information."
+		if !strings.Contains(gotBody, wantBody) {
+			t.Fatalf("Body = %v; want substring %v", gotBody, wantBody)
 		}
 	})
 }
