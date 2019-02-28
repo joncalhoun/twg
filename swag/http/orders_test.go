cafc597866b9016eafbb5a8be52c4c91071bf71b
Jon Calhoun <joncalhoun@gmail.com>
2019-02-28 13:13:20 -0500

    Test: Order middleware

--- swag/http/orders_test.go
+++ swag/http/orders_test.go
@@ -2,6 +2,7 @@ package http_test
 
 import (
 	"context"
+	"database/sql"
 	"fmt"
 	"html/template"
 	"io/ioutil"
@@ -138,6 +139,75 @@ func TestOrderHandler_Create(t *testing.T) {
 		wantLoc := fmt.Sprintf("/orders/%s", stripeCustomerID)
 		if gotLoc != wantLoc {
 			t.Fatalf("Redirect location = %s; want %s", gotLoc, wantLoc)
+		}
+	})
+}
+
+func TestOrderHandler_OrderMw(t *testing.T) {
+	failHandler := func(t *testing.T) http.HandlerFunc {
+		return func(w http.ResponseWriter, r *http.Request) {
+			t.Fatalf("next handler shouldn't have been called by middleware")
+		}
+	}
+	// Three paths:
+	// 1. Can't find order or DB error
+	// 2. Success
+	t.Run("missing order", func(t *testing.T) {
+		oh := OrderHandler{}
+		mdb := &mockDB{
+			GetOrderViaPayCusFunc: func(id string) (*db.Order, error) {
+				return nil, sql.ErrNoRows
+			},
+		}
+		oh.DB = mdb
+		handler := oh.OrderMw(failHandler(t))
+		w := httptest.NewRecorder()
+		r := httptest.NewRequest(http.MethodGet, "/cus_abc123/id/here", nil)
+		handler(w, r)
+		res := w.Result()
+		if res.StatusCode != http.StatusNotFound {
+			t.Fatalf("StatusCode = %d; want %d", res.StatusCode, http.StatusNotFound)
+		}
+	})
+
+	t.Run("order found", func(t *testing.T) {
+		order := &db.Order{
+			ID: 123,
+			Payment: db.Payment{
+				CustomerID: "cus_abc123",
+				Source:     "stripe",
+			},
+		}
+		oh := OrderHandler{}
+		mdb := &mockDB{
+			GetOrderViaPayCusFunc: func(id string) (*db.Order, error) {
+				if id == order.Payment.CustomerID {
+					return order, nil
+				}
+				return nil, sql.ErrNoRows
+			},
+		}
+		oh.DB = mdb
+		handlerCalled := false
+		handler := oh.OrderMw(func(w http.ResponseWriter, r *http.Request) {
+			handlerCalled = true
+			if r.URL.Path != "/remaining/path/" {
+				t.Fatalf("Path in next handler = %v; want %v", r.URL.Path, "/remaining/path/")
+			}
+			got := r.Context().Value("order").(*db.Order)
+			if got != order {
+				t.Fatalf("Order = %v; want %v", got, order)
+			}
+		})
+		w := httptest.NewRecorder()
+		r := httptest.NewRequest(http.MethodGet, fmt.Sprintf("/%s/remaining/path", order.Payment.CustomerID), nil)
+		handler(w, r)
+		res := w.Result()
+		if res.StatusCode != http.StatusOK {
+			t.Fatalf("StatusCode = %d; want %d", res.StatusCode, http.StatusOK)
+		}
+		if !handlerCalled {
+			t.Fatalf("next handler not called")
 		}
 	})
 }
